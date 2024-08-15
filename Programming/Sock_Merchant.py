9d7d7b4737099cfb13906e8659f50e12db61c580
Steven-Minuk <minuk4688@gmail.com>
2024-08-14 23:58:28 -0500

    Add answer for Top Competitors

--- Programming/Sock_Merchant.py
+++ Programming/Sock_Merchant.py
@@ -1,30 +1,29 @@
 """
 Sock Merchant - Sales by Match
-
-There is a large pile of socks that must be paired by color.
-Given an array of integers representing the color of each sock, determine how many pairs of socks with matching colors there are.
+    There is a large pile of socks that must be paired by color.
+    Given an array of integers representing the color of each sock, determine how many pairs of socks with matching colors there are.
 
 Example
-n = 7
-ar = [1, 2, 1, 2, 1, 3, 2]
-There is one pair of color 1 and one of color 2. There are three odd socks left, one of each color. The number of pairs is 2.
+    n = 7
+    ar = [1, 2, 1, 2, 1, 3, 2]
+    There is one pair of color 1 and one of color 2. There are three odd socks left, one of each color. The number of pairs is 2.
 
 Function Description
-Complete the sockMerchant function in the editor below.
-sockMerchant has the following parameter(s):
- - int n: the number of socks in the pile
- - int ar[n]: the colors of each sock
+    Complete the sockMerchant function in the editor below.
+    sockMerchant has the following parameter(s):
+     - int n: the number of socks in the pile
+     - int ar[n]: the colors of each sock
 
 Return
- - int: the number of pairs
+     - int: the number of pairs
 
 Input Format
-The first line contains an integer n, the number of socks represented in ar.
-The second lint contains n space-separated integers, ar[i], the colors of the socks in the pile.
+    The first line contains an integer n, the number of socks represented in ar.
+    The second lint contains n space-separated integers, ar[i], the colors of the socks in the pile.
 
 Contraints
- - 1 <= n <= 100
- - 1 <= ar[i] <= 100 where 0 <= i < n
+     - 1 <= n <= 100
+     - 1 <= ar[i] <= 100 where 0 <= i < n
 """
 
 import math
