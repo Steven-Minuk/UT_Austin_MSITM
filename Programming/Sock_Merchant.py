ef0df42511a43623ed37d98a2e5aedc5a4a08e40
Steven-Minuk <minuk4688@gmail.com>
2024-08-15 00:30:51 -0500

    Add answer for Sock Merchant

--- Programming/Sock_Merchant.py
+++ Programming/Sock_Merchant.py
@@ -26,33 +26,15 @@ Contraints
      - 1 <= ar[i] <= 100 where 0 <= i < n
 """
 
-import math
-import os
-import random
-import re
-import sys
-
-#
-# Complete the 'sockMerchant' function below.
-#
-# The function is expected to return an INTEGER.
-# The function accepts following parameters:
-#  1. INTEGER n
-#  2. INTEGER_ARRAY ar
-#
-
+# Answer
 def sockMerchant(n, ar):
-    # Write your code here
-
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-    n = int(input().strip())
-
-    ar = list(map(int, input().rstrip().split()))
-
-    result = sockMerchant(n, ar)
-
-    fptr.write(str(result) + '\n')
-
-    fptr.close()
+    ar.sort()
+    index = 0
+    pair = 0
+    while index < n - 1:
+        if ar[index] == ar[index + 1]:
+            pair += 1
+            index += 2
+        else:
+            index += 1
+    return pair
